3183e662b958d2be8a6f725502d4f97595d9ab71
arpitgithub07072005 <arpitpandey07072005@gmail.com>
2025-08-28 22:17:18 +0530

    jai ho

--- src/components/Quizlanding.jsx
+++ src/components/Quizlanding.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../css/Quizlanding.module.css'
+import styles from '../CSS/Quizlanding.module.css'
 import { useNavigate } from 'react-router'
 
 const Quizlanding = () => {
